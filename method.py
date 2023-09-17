4e7090ee5357aa3a08bab58141ff8cfe48a093c8
TeddyDiallo <teddy.f.diallo-1@ou.edu>
2023-09-17 15:57:14 -0500

    Added comments in part10

--- method.py
+++ method.py
@@ -1,9 +1,11 @@
+#I am a genius at Git
 def sum_list(mylist):
     sum = 0
     for i in mylist:
         sum += i
     return sum 
 
+#I love testing stuff
 def product_list(mylist):
     product = 1
     for i in mylist:
